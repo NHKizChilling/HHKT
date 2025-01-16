20f70558c89a83f188bbd05f56e83428df77b95f
truong0910 <161480441+truong0910@users.noreply.github.com>
2025-01-17 01:10:02 +0700

    test crud KhachHang

---
+++ HHKT_Station/src/main/java/test_crud/Test_KhachHang.java
@@ -0,0 +1,29 @@
+package test_crud;
+
+import dao.KhachHang_DAO;
+import entity.KhachHang;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+
+public class Test_KhachHang {
+    public static void main(String[] args) {
+        EntityManager em = Persistence.createEntityManagerFactory("HHKT Station").createEntityManager();
+        KhachHang_DAO khachHang_dao = new KhachHang_DAO(em);
+
+        try{
+            KhachHang kh = new KhachHang("Nguyen Van A", "12345678912", "0223456789", "nguyenvana@gmail.com");
+            khachHang_dao.creat(kh);
+
+            System.out.println("Khách hàng vừa thêm: " + khachHang_dao.getAllKhachHang().getLast().getMaKH() + " - " + khachHang_dao.getAllKhachHang().getLast().getTenKH()
+                    + " - " + khachHang_dao.getAllKhachHang().getLast().getSdt() + " - " + khachHang_dao.getAllKhachHang().getLast().getEmail());
+            KhachHang khbefore = khachHang_dao.getKhachHangTheoMaKH(khachHang_dao.getAllKhachHang().getLast().getMaKH());
+            KhachHang khafter = new KhachHang();
+            if(khachHang_dao.updateSoSDT(khbefore.getMaKH(), "0123456789")){
+                khafter = khachHang_dao.getKhachHangTheoMaKH(khachHang_dao.getAllKhachHang().getLast().getMaKH());
+            }
+            System.out.println("Sau khi update số điện thoại: " + khafter.getMaKH() + " - " + khafter.getSdt());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
